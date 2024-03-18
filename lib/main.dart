3e84b3d9e61b1ca83206fde4bb69bafc39374f14
Tishan-001 <tishanshamika200@gmail.com>
2024-03-18 10:01:39 +0530

    firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pre_school/firebase_options.dart';
 import 'package:pre_school/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyB8N1SBAv__hB9H5Mo2Bt3StYj0uICUt7s", // API Key
-      appId: "1:169025738096:android:e4ceb227d3b54fc2c9abb5", // App ID
-      messagingSenderId: "169025738096", // Sender ID
-      projectId: "pre-school-88b37", // Project ID
-      storageBucket: "pre-school-88b37.appspot.com", // Storage Bucket
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
@@ -26,5 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// If there any problem please contact +94743399656 (WhatsApp)
